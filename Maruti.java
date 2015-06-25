2f2618f28e053e8bbe2824de10a769488ec34dd3
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 19:05:27 +0530

    Create Maruti.java

---
+++ Maruti.java
@@ -0,0 +1,13 @@
+package com.csc.designpattern.creational;
+
+public class Maruti implements Car {
+
+	@Override
+	public void getCarName() {
+		System.out.println("This is Maruthi Car");
+	}
+
+
+
+
+}
