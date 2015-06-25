939fbb353d541923f6691192cdbeb80fbfd683e8
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 18:34:04 +0530

    TestSingleTonExample.java

---
+++ TestSingleTonExample.java
@@ -0,0 +1,21 @@
+package com.csc.designpattern.creational;
+
+public class TestSingleTonExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(SingleTonEx.getInstance());
+		System.out.println(SingleTonEx.getInstance());
+		System.out.println(SingleTonEx.getInstance());
+		System.out.println(SingleTonEx.getInstance());
+		System.out.println(SingleTonEx.getInstance());
+		System.out.println(SingleTonEx.getInstance());
+		
+
+	}
+
+}
+
+
