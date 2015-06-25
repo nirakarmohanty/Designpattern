08fea797f296e7d69742041605aa364f43b05cb8
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 19:06:21 +0530

    Create CarFactory.java

---
+++ CarFactory.java
@@ -0,0 +1,35 @@
+package com.csc.designpattern.creational;
+
+/**
+ * This is a factory class which will return the different class name at run
+ * time
+ * 
+ * @author nmohanty
+ * 
+ */
+public class CarFactory {
+
+	private static CarFactory carFactoryObject = new CarFactory();
+
+	/**
+	 * Singleton implementation
+	 * 
+	 * @return
+	 */
+	public static CarFactory getInstance() {
+		return carFactoryObject;
+	}
+
+	public Car getInstace(String carName) {
+
+		if (carName.equalsIgnoreCase("maruthi"))
+			return new Maruti();
+		if (carName.equalsIgnoreCase("mahindra"))
+			return new Mahindra();
+		if (carName.equalsIgnoreCase("hundai"))
+			return new Hundai();
+		else
+			return null;
+	}
+
+}
