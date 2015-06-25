7566c17f3a3a8a0d82bcd2f176dd665ee1511413
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 19:05:59 +0530

    Create Hundai.java

---
+++ Hundai.java
@@ -0,0 +1,11 @@
+package com.csc.designpattern.creational;
+
+public class Hundai implements Car{
+
+	@Override
+	public void getCarName() {
+		System.out.println("Hi this is Hundai Car");
+		
+	}
+
+}
