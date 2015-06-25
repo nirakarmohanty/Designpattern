a65bc86e360e9f026ad8f9242b8562a975bcb7dc
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 19:06:38 +0530

    Create TestFactoryExample.java

---
+++ TestFactoryExample.java
@@ -0,0 +1,23 @@
+package com.csc.designpattern.creational;
+
+public class TestFactoryExample {
+
+	/** This is a tester class to test Factory design pattern 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		CarFactory carfactoryObj = CarFactory.getInstance();
+		System.out.println("---------------------");
+		Car carObject1 = carfactoryObj.getInstace("maruthi");
+		carObject1.getCarName();
+		System.out.println("----- Maruthi class Instantiate -------------");
+		Car carObject2 = carfactoryObj.getInstace("hundai");
+		carObject2.getCarName();
+		System.out.println("----- Hundai class Instantiate -------------");
+		Car carObject3 = carfactoryObj.getInstace("mahindra");
+		carObject3.getCarName();
+		System.out.println("----- Mahindra class Instantiate -------------");
+	}
+
+}
