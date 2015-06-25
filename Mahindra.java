ac2a9d9c9a23b471f0f2f6abdb6ddafdd99d0d2a
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-06-25 19:05:46 +0530

    Create Mahindra.java

---
+++ Mahindra.java
@@ -0,0 +1,13 @@
+package com.csc.designpattern.creational;
+
+public class Mahindra implements Car {
+
+	@Override
+	public void getCarName() {
+		System.out.println("Hi this is Mahindra Car");
+		
+	}
+
+	
+
+}
